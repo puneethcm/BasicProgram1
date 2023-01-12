48d7ea74ee64b4b46089c1147b4200910f05dd01
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-12 15:27:13 +0530

    addition

---
+++ BasicProgram/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BasicProgram
+{
+	internal class Calculator
+	{
+        public int first, second, result;
+        public Calculator(int firstNum, int secondNum)
+        {
+            first = firstNum;
+            second = secondNum;
+        }
+        /// <summary>
+        /// Addition of 2 num to get result
+        /// </summary>
+        public void Addition()
+        {
+            result = first + second;
+            Console.WriteLine("Sum:" + result);
+        }
+    }
+}
+
--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -3,7 +3,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Basic Program");
+        Console.WriteLine("Please enter first number");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter second number");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Calculator calculator = new Calculator(first, second);
+        calculator.Addition();
+        Console.ReadLine();
     }
 }
 
