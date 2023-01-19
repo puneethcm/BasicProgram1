f4b5406bae14b075a6d150a31bcdb20fadaf4e91
puneethcm <120664786+puneethcm@users.noreply.github.com>
2023-01-19 09:39:34 +0530

    Update FiboNumber.cs
    modified

--- BasicProgram/FiboNumber.cs
+++ BasicProgram/FiboNumber.cs
@@ -12,9 +12,9 @@ namespace BasicProgram
 		public void Fibonacci()
 		{
 			int n1 = 0, n2 = 1, n3, i;
-            Console.Write("Fibonacci Numbers: ");
+            		Console.Write("Fibonacci Numbers are: ");
 			Console.Write(n1 + "" + n2 + "");
-            for (i = 2; i < number; i++)
+            		for (i = 2; i < number; i++)
 			{
 				n3 = n1 + n2;
 				Console.Write(n3 + "");
