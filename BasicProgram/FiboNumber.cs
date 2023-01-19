53309b10cec027171242babe9d4dc35c3162b838
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-19 09:41:39 +0530

    Modified1

--- BasicProgram/FiboNumber.cs
+++ BasicProgram/FiboNumber.cs
@@ -12,13 +12,13 @@ namespace BasicProgram
 		public void Fibonacci()
 		{
 			int n1 = 0, n2 = 1, n3, i;
-            		Console.Write("Fibonacci Numbers are: ");
+            Console.Write("Fibonacci Numbers are: ");
 			Console.Write(n1 + "" + n2 + "");
-            		for (i = 2; i < number; i++)
+            for (i = 2; i < number; i++)
 			{
 				n3 = n1 + n2;
 				Console.Write(n3 + "");
-				//initializing
+				//initializing n1 value to n2
 				n1 = n2;
 				n2 = n3;
 			}
