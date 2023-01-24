46722f6bcb91608920fe2237fe634b738a928e8f
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-24 09:33:29 +0530

    modified

--- BasicProgram/CoinFlip.cs
+++ BasicProgram/CoinFlip.cs
@@ -21,9 +21,9 @@ namespace BasicProgram
 					tailCount++;
 				}	
 			}
-            		Console.WriteLine("Head {0} Tail {1}", headCount, tailCount);
+            Console.WriteLine("Head {0} Tail {1}", headCount, tailCount);
 			int headPercent = (headCount * 100) / n;
-            		Console.WriteLine("Head Percentage {0}%", headPercent);
+            Console.WriteLine("Head Percentage {0}%", headPercent);
 			int tailPercent = (tailCount * 100) / n;
             		Console.WriteLine("Head Percentage {0}%", tailPercent);
         }
