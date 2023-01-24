4bca8c0232e3699835a898be56f37531b911e738
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-24 10:48:22 +0530

    modified

--- BasicProgram/SwapNumber.cs
+++ BasicProgram/SwapNumber.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 namespace BasicProgram
 {
-	public class SwapNumber
-	{
-		public static void SwapTowNumber()
-		{
-			int number = 0;
-			Console.WriteLine("Enter the First Number: ");
-			int first = Convert.ToInt32(Console.ReadLine());
-            		Console.WriteLine("Enter the Second Number: ");
-            		int second = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine("Numbers Before Swapping ===> {0}, {1}", first, second);
-            		number = first;
-			first = second;
-			second = number;
-            		Console.WriteLine("Numbers After Swapping ===> {0}, {1}", first, second);
-        	}
-	}
+    public class SwapNumber
+    {
+        public static void SwapTowNumber()
+        {
+            int number = 0;
+            Console.WriteLine("Enter the First Number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numbers Before Swapping ===> {0}, {1}", first, second);
+            number = first;
+            first = second;
+            second = number;
+            Console.WriteLine("Numbers After Swapping ===> {0}, {1}", first, second);
+        }
+    }
 }
-
