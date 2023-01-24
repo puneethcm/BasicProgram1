f85250eb7e5bc8029e91a1421a66a5a5ac0b9267
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-24 19:49:58 +0530

    Vowel or Consonant

---
+++ BasicProgram/VowelOrconsonant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace BasicProgram
+{
+	public class VowelOrconsonant
+	{
+		public static void VowelOrConsonants()
+		{
+			Console.WriteLine("Input any Alphabet from (A to Z) or (a to z)");
+			char ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+			if(i>=0 && i<=57)
+			{
+				Console.WriteLine("You Entered a Number Please, Enter an Alphabet");
+			}
+			else
+			{
+				switch (ch)
+				{
+					case 'a':
+						Console.WriteLine("The Alphabet is vowel");
+						break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+					default:
+						Console.WriteLine("The Alphabet is Consonant");
+						break;
+                }
+			}
+		}
+	}
+}
+
