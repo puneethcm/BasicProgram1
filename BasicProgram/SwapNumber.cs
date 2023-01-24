606ffa67c89095510a1e8fb2897c743137416115
puneethcm <120664786+puneethcm@users.noreply.github.com>
2023-01-24 10:46:39 +0530

    Update SwapNumber.cs

--- BasicProgram/SwapNumber.cs
+++ BasicProgram/SwapNumber.cs
@@ -8,14 +8,14 @@ namespace BasicProgram
 			int number = 0;
 			Console.WriteLine("Enter the First Number: ");
 			int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Second Number: ");
-            int second = Convert.ToInt32(Console.ReadLine());
+            		Console.WriteLine("Enter the Second Number: ");
+            		int second = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Numbers Before Swapping ===> {0}, {1}", first, second);
-            number = first;
+            		number = first;
 			first = second;
 			second = number;
-            Console.WriteLine("Numbers After Swapping ===> {0}, {1}", first, second);
-        }
+            		Console.WriteLine("Numbers After Swapping ===> {0}, {1}", first, second);
+        	}
 	}
 }
 
