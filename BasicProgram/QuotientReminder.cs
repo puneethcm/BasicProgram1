e8c2861dfe7ba63f40a07586c77e0a6ca4d91c19
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-29 18:17:13 +0530

    Quotient and remainder

---
+++ BasicProgram/QuotientReminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BasicProgram
+{
+	public class QuotientReminder
+	{
+		public static void FindQuotientReminder()
+		{
+            Console.WriteLine("Enter the divident");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+            Console.WriteLine("Divident:{0} Divisor:{1}", divident, divisor);
+            Console.WriteLine("Quotient=:" + quotient);
+            Console.WriteLine("Remainder=:" + remainder);
+            Console.ReadLine();
+        }
+	}
+}
+
