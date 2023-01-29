b225f8dfeef0e8056c536ce17826dd3384a8e4fe
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-29 18:08:06 +0530

    Larger number

---
+++ BasicProgram/LargerNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BasicProgram
+{
+	public class LargerNumber
+	{
+		public static void FindLargerNumber()
+		{
+            Console.WriteLine("Enter the Number to find larger number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c)
+                Console.WriteLine("{0} is greter Number",a);
+            else if (b > a && b > c)
+                Console.WriteLine("{0} is greter Number",b);
+            else
+                Console.WriteLine("{0} is greater",c);
+            Console.ReadLine();
+        }
+	}
+}
+
--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -9,7 +9,7 @@ class Program
         Console.WriteLine("Please choose any one program");
         Console.WriteLine("1:Calculator\n2:SwitchCase\n3:Fibonacci\n4:PrimeNumber\n5:Coin Flip\n6:Swap two numbers" +
                            "\n7:Enen and odd numbers\n8:To Fine Number is Prime or Not\n9:Vowels or Consonant\n10:Leap Year" +
-                           "\n11:Harmonic Numbers\n:12:Fotorial");
+                           "\n11:Harmonic Numbers\n:12:Fotorial\n13:Large Number");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -85,6 +85,9 @@ class Program
                 break;
             case 12:
                 Factorial.FindFactorial();
+                break;
+            case 13:
+                LargerNumber.FindLargerNumber();
                 break;
 
             default:
